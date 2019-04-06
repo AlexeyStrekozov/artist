78b6aa90fa44958fd58de6b3b28b148fe43a8148
AlexeyStrekozov <strekozovalexey@gmail.com>
2019-04-07 02:42:52 +0600

    Сделал адаптив

--- script/map.js
+++ script/map.js
@@ -3,6 +3,7 @@ var map;
         map = DG.map('map', {
             center: [55.430298, 65.34104500000001],
             zoom: 19,
+            dragging: false,
             scrollWheelZoom: false
         });
         DG.marker([55.43036952960665, 65.34104768220902]).addTo(map).bindPopup('Рекламное агентство');
