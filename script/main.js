9b4e99da6917084d91424bf420702b17c08c4311
AlexeyStrekozov <strekozovalexey@gmail.com>
2019-04-07 22:33:05 +0600

    Меню

--- script/main.js
+++ script/main.js
@@ -1,3 +1,10 @@
+$( document ).ready(function(){
+  $( ".header-navigation__button" ).click(function(){ // задаем функцию при нажатиии на элемент с классом slide-toggle
+    $( ".header-navigation__minibar" ).slideToggle(); // плавно скрываем, или отображаем все элементы <div>
+  });
+});
+
 $(function(){
     $(".phone").mask("+7(999) 999-9999");
   });
+
